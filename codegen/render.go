46d4559246a43fb67a4a3c3e4b2c9e1fc36fcd1e
Tomas Bartkus <to.bartkus@gmail.com>
2016-02-07 17:17:26 +0200

    Add render func

--- codegen/render.go
+++ codegen/render.go
@@ -2,12 +2,17 @@ package codegen
 
 import (
 	"io"
+	"reflect"
 	"text/template"
 )
 
 // Render method
 func Render(w io.Writer, tpl string, data interface{}) error {
-	templateFuncs := template.FuncMap{}
+	templateFuncs := template.FuncMap{
+		"last": func(x int, a interface{}) bool {
+			return x == reflect.ValueOf(a).Len()-1
+		},
+	}
 	tplParsed := template.Must(template.New("render").Funcs(templateFuncs).Parse(tpl))
 	return tplParsed.Execute(w, data)
 }
